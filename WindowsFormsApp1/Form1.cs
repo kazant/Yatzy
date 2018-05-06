7e4c81a271b7ca5c4962b180c8d8f8ec5db3ef28
Alexander Pettersen <Alex1990pettersen@gmail.com>
2018-05-06 14:45:57 +0200

    fjernet ubrukt kode

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using static WindowsFormsApp1.YatzyKategoriBeregner;
 
 namespace WindowsFormsApp1
 {
-   
+
     public partial class Form1 : Form
     {
         private const int CHECKBOXNUMMEREN = 0;
